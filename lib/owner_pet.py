ec2e57ad36838c1bf4c3d9364c411fdc404b3eb0
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-31 01:59:53 +0300

    One-to-many Object Relationships Lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,47 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The pet must be an instance of the Pet class.")
+        if pet not in self._pets:
+            self._pets.append(pet)
+            pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is not a valid pet type.")
+        self.name = name
+        self.pet_type = pet_type
+        self._owner = None
+        self.owner = owner  # This will call the owner setter method
+        Pet.all.append(self)
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("The owner must be an instance of the Owner class.")
+        if self._owner is not owner:
+            if self._owner:
+                self._owner._pets.remove(self)
+            self._owner = owner
+            if owner:
+                owner.add_pet(self)
+
